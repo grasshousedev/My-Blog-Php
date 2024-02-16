53b31c12c647aa98add68b4cabb7265a6a25dc57
Sasha dev <boltbright525@gmail.com>
2024-02-16 22:04:21 +0200

    base posts admin page

--- admin/posts/index.php
+++ admin/posts/index.php
@@ -10,7 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="../../assets/css/admin.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.  com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Montserrat:wght@400;800&family=Roboto&display=swap"
           rel="stylesheet">
@@ -21,7 +21,7 @@
 <?php include("../../app/include/header_admin.php"); ?>
 <div class="container">
     <div class="row">
-        <div class="sidebar col-4">
+        <div class="sidebar col-3">
             <ul>
                 <li>
                     <a href="">Записи</a>
@@ -34,20 +34,25 @@
                 </li>
             </ul>
         </div>
-        <div class="posts col-8">
+        <div class="posts col-9">
+            <div class="button row">
+                <a class="btn btn-success col-3" href="create.php">Добавить пост</a>
+                <span class="col-1"></span>
+                <a class="btn btn-warning col-3" href="index.php">Изменить посты</a>
+            </div>
             <div class="row title-table">
-                <div class="id col-1">ID</div>
-                <div class="title col-5">Название</div>
-                <div class="author col-2">Автор</div>
-                <div class="red col-2">Редактировать</div>
-                <div class="del col-2">Удалить</div>
+                <h2>Управление записями</h2>
+                <div class="col-1">ID</div>
+                <div class="col-5">Название</div>
+                <div class="col-2">Автор</div>
+                <div class="col-4">Управление</div>
             </div>
             <div class="row post">
                 <div class="id col-1">1</div>
                 <div class="title col-5">Какая-то статья</div>
                 <div class="author col-2">Admin</div>
-                <div class="red col-2"><a href="">Edit</a></div>
-                <div class="del col-2"><a href="">Delete</a></div>
+                <div class="edit col-2"><a href="">Edit</a></div>
+                <div class="delete col-2"><a href="">Delete</a></div>
             </div>
         </div>
     </div>
